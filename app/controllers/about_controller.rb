2e3ab927637675efcfaf57db1a65142658865d53
Rene Mercadel <renemercadel@gmail.com>
2022-10-13 16:33:19 -0500

    first commit Beginner Ruby course

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,7 @@
+class AboutController < ApplicationController
+  
+  # I dont need to define anything in the "index" action since its being inherited the rails default renderfrom the ApplicationController
+  def index
+  end
+
+end
