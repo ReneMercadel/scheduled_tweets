c90a17e1bed620d914339b52b6d78284dab5dd67
Rene Mercadel <renemercadel@gmail.com>
2022-10-13 17:03:54 -0500

    adding flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,7 @@
 class MainController < ApplicationController
   def index
-    
+    # flash is inherited from the rails Application controller
+    flash[:notice] = "Logged in successfully"
+    flash[:alert] = "Invalid Login"
   end
 end
